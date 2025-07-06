3a7985616da8cb43e25a5b40a917c6a8686bdfd5
Aelithron <aelithron@gmail.com>
2025-07-06 01:40:15 -0600

    work on lookup a little

--- commands/journal/lookup.ts
+++ commands/journal/lookup.ts
@@ -1,8 +1,30 @@
-import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { CommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
+import userCollection from "../../utils/db";
+import { ObjectId } from "mongodb";
+import { UserJournal } from "../../petalnote";
 
 export const data = new SlashCommandBuilder()
   .setName('lookup')
-  .setDescription('look up a journal entry');
+  .setDescription('look up a journal entry')
+  .addSubcommand(subcommand =>
+		subcommand
+			.setName('date')
+			.setDescription('date when the entries were added')
+			.addStringOption(option => option.setName('date').setDescription('a date')));
 export async function execute(interaction: CommandInteraction) {
-  interaction.reply("this command is under development!");
-}
+  await interaction.deferReply({ flags: MessageFlags.Ephemeral })
+  let userDoc = await userCollection.findOne({ userID: interaction.user.id }) as UserJournal;
+  if (!userDoc) {
+    userDoc = {
+      _id: new ObjectId(),
+      userID: interaction.user.id,
+      entries: []
+    } as UserJournal;
+    await userCollection.insertOne(userDoc);
+  }
+  if (userDoc.entries.length === 0) {
+    await interaction.reply("you don't have any journal entries!");
+    return;
+  }
+  await interaction.reply("under development!");
+};
