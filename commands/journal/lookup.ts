3c6380ff58d2c03f5f96fa407b06437972fb2239
Aelithron <aelithron@gmail.com>
2025-07-08 04:29:24 -0600

    Add basic logic for lookup Allows it to pull from the database. That's basically it for that command so far :3

--- commands/journal/lookup.ts
+++ commands/journal/lookup.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, MessageFlags, SlashCommandBuilder } from "discord.js";
+import { APIEmbedField, CommandInteraction, EmbedBuilder, MessageFlags, RestOrArray, SlashCommandBuilder } from "discord.js";
 import userCollection from "../../utils/db";
 import { ObjectId } from "mongodb";
 import { UserJournal } from "../../petalnote";
@@ -6,11 +6,9 @@ import { UserJournal } from "../../petalnote";
 export const data = new SlashCommandBuilder()
   .setName('lookup')
   .setDescription('look up a journal entry')
-  .addSubcommand(subcommand =>
-		subcommand
-			.setName('date')
-			.setDescription('date when the entries were added')
-			.addStringOption(option => option.setName('date').setDescription('a date')));
+  .addStringOption(option =>
+    option.setName('date')
+      .setDescription('date when the entries were added'));
 export async function execute(interaction: CommandInteraction) {
   await interaction.deferReply({ flags: MessageFlags.Ephemeral })
   let userDoc = await userCollection.findOne({ userID: interaction.user.id }) as UserJournal;
@@ -27,5 +25,23 @@ export async function execute(interaction: CommandInteraction) {
     return;
   };
   
-  await interaction.editReply({ content: 'under development!' });
+  // TODO: Add filtering logic based on command options
+
+  const embed = new EmbedBuilder()
+    .setColor(0x7932a8)
+		.setTitle(`Journal Entries`)
+  const entries = userDoc.entries; // later: change to "const entries = await entriesPerPage(userDoc.entries, 1);"
+  const fields: RestOrArray<APIEmbedField> = [];
+  for (const entry of entries) fields.push({ name: entry.createdAt.toDateString(), value: `**Mood:** ${entry.mood}\n${entry.text ? entry.text : '*No text entered*'}` });
+  embed.addFields(fields);
+  await interaction.editReply({ embeds: [embed] });
 };
+
+/*
+  async function entriesPerPage(entries: JournalEntry[], page: number): Promise<JournalEntry[]> {
+    const pageEntries: JournalEntry[];
+    const startingIndex = page * 5;
+    for () {}
+    return pageEntries;
+  }
+*/
